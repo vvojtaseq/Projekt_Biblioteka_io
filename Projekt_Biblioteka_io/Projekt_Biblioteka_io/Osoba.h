4fad02a69f007b9afb7d608a48cfe29fa2d73f5d
Marysia Wojtas <mania.wojtas@gmail.com>
2023-12-01 14:06:35 +0100

    metody do klasy Osoba

--- Projekt_Biblioteka_io/Projekt_Biblioteka_io/Osoba.h
+++ Projekt_Biblioteka_io/Projekt_Biblioteka_io/Osoba.h
@@ -1,14 +1,37 @@
 #pragma once
 #include "Includy.h"
+#define MAXPESEL 99999999999
+#define MINPESEL 10000000000
 class Osoba
 {
-public:
+private:
 	std::string Imie;
 	std::string Nazwisko;
 	std::string Adres;
-	int Telefon;
-	int PESEL;
 	std::string DataUrodzenia;
-private:
+	int Telefon;
+	long long PESEL;
+	
+public:
+	//metody 
+	//trzeba zrobic metody get i set, set potrzebne do tworzenia uzytkownika, get do logowania i innych klas 
+	std::string getImie() { return Imie; }
+	void setImie(std::string noweImie) { Imie = noweImie; }
+	std::string getNazwisko() { return Nazwisko; }
+	void setNazwisko(std::string noweNazwisko) { Nazwisko = noweNazwisko; }
+	std::string getAdres() { return Adres; }
+	void setAdres(std::string nowyAdres) { Adres = nowyAdres; }
+	std::string getDataUrodzenia() { return DataUrodzenia; }
+	long long getPESEL() { return PESEL; }
+	void setPESEL(long long nowyPESEL) 
+	{
+		if (nowyPESEL > MAXPESEL || nowyPESEL < MINPESEL)
+		{
+			std::cout << "Niepoprawny nr PESEL, wprowadŸ jeszcze raz. \nNowy PESEL : ";
+			nowyPESEL = 0;
+				std::cin >> nowyPESEL;
+				setPESEL(nowyPESEL);
+		}
+	}
 };
 
