9fee1e2c13960d906193e0dfe9220102baa292d4
WojZacharski <wojzacharski@student.agh.edu.pl>
2023-12-27 16:32:12 +0100

    Dodanie plików do testu jednostkowego

---
+++ Projekt_Biblioteka_io/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "main.cpp"
+
+//Jeœli nie dzia³a to w zak³adce 
+//Projekt / W³aœciwoœci UnitTest1 / C/C++ / Ogólne / Dodatkowe Plki Katalogów nag³owkowych
+//Dodaæ œcie¿kê w której znajduje siê ,,Projekt_Biblioteka_io"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
+ 
