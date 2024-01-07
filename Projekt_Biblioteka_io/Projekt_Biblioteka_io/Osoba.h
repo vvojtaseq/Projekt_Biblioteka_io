aa8f84bf62f8b4389210e2137b91af01e20a7701
WojZacharski <wojzacharski@student.agh.edu.pl>
2024-01-07 23:21:44 +0100

    Merge

--- Projekt_Biblioteka_io/Projekt_Biblioteka_io/Osoba.h
+++ Projekt_Biblioteka_io/Projekt_Biblioteka_io/Osoba.h
@@ -21,38 +21,39 @@ public:
 	//metody 
 	//trzeba zrobic metody get i set, set potrzebne do tworzenia uzytkownika, get do logowania i innych klas 
 	std::string getImie() { return Imie; }
-	void setImie(std::string noweImie) { Imie = noweImie; }
+	void setImie(std::string noweImie)
+	{ 
+		Imie = noweImie;
+		//std::cout << Imie << std::endl;
+	}
 	std::string getNazwisko() { return Nazwisko; }
-	void setNazwisko(std::string noweNazwisko) { Nazwisko = noweNazwisko; }
+	void setNazwisko(std::string noweNazwisko) 
+	{ 
+		Nazwisko = noweNazwisko;
+		//std::cout << Nazwisko << std::endl;
+	}
 	std::string getAdres() { return Adres; }
-	void setAdres(std::string nowyAdres) { Adres = nowyAdres; }
+	void setAdres(std::string nowyAdres)
+	{
+		Adres = nowyAdres;
+		//std::cout << Adres << std::endl;
+	}
 	std::string getDataUrodzenia() { return DataUrodzenia; }
-	void setDataUrodzenia(std::string nowaDataUrodzenia) { DataUrodzenia = nowaDataUrodzenia; }
+	void setDataUrodzenia(std::string nowaDataUrodzenia) 
+	{ 
+		DataUrodzenia = nowaDataUrodzenia;
+		//std::cout << DataUrodzenia << std::endl; 
+	}
 	std::string getTelefon() { return Telefon; }
-	void setTelefon(std::string nowyTelefon) { Telefon = nowyTelefon; }
+	void setTelefon(std::string nowyTelefon)
+	{
+		Telefon = nowyTelefon;
+		//std::cout << Telefon << std::endl;
+	}
 	std::string getPESEL() { return PESEL; }
-	void setPESEL(std::string nowyPESEL) {
-		while (true) {
-			if (nowyPESEL.length() != PESELLENGTH) {
-				std::cout << "Niepoprawna dlugosc numeru PESEL. Wprowadz 11 znakow." << std::endl;
-			}
-			else if (!std::all_of(nowyPESEL.begin(), nowyPESEL.end(), ::isdigit)) {
-				std::cout << "Numer PESEL powinien zawierac tylko cyfry." << std::endl;
-			}
-			else {
-				long long pesel = std::stoll(nowyPESEL);
-				if (pesel > MAXPESEL || pesel < MINPESEL) {
-					std::cout << "Niepoprawny nr PESEL. Numer PESEL powinien zawierac 11 cyfr." << std::endl;
-				}
-				else {
-					PESEL = nowyPESEL;
-					//std::cout << "Nowy pesel to: " << PESEL << std::endl; wyœwietla podczas wprowadzania
-					break; // Exit the loop after setting the valid PESEL
-				}
-			}
-			std::cout << "Wprowadz nowy PESEL: ";
-			std::cin >> nowyPESEL;
-		}
+	void setPESEL(std::string nowyPESEL)
+	{
+		PESEL = nowyPESEL;
 	}
 
 	unsigned int getID() { return ID; }
--- Projekt_Biblioteka_io/Projekt_Biblioteka_io/UI.cpp
+++ Projekt_Biblioteka_io/Projekt_Biblioteka_io/UI.cpp
@@ -1,12 +1,13 @@
 #include "Includy.h"
 #include "IncludyKlasy.h"
 #define HASLOPRACOWNIKOW = 88987
+
 void userInterface()
 {
 	system("CLS");
 	std::cout << "**<< Biblioteka dla Studentów >>**" << std::endl << std::endl << std::endl;
-	std::cout << "Opcje :\n";
-	std::cout << "1 - U¿ytkownik		2 - Pracownik " << std::endl;
+	std::cout << "Opcje :"<<std::endl;
+	std::cout << "[1] - U¿ytkownik\t\t[2] - Pracownik " << std::endl;
 	// to bêdzie po wybraniu jakis if czy cos, i beda dwie rozne opcje , jedna dla uzytkownika, druga dla pracownika
 	int decide;
 	std::cin >> decide;
@@ -19,17 +20,17 @@ void userInterface()
 		userInterfacePracownik();
 		break;
 	default:
-		std::cout << "Niepoprawne dane, spróbuj jeszcze raz" << std::endl;
+		std::cout << "Opcja o id: "<<decide<<" nie istnieje! Spróbuj ponownie!" << std::endl;
 		system("pause");
-		userInterface();
+		userInterfaceUzytkownik();
 	}
 }
 void userInterfaceUzytkownik()
 {
 	system("CLS");
 	std::cout << "**<< Biblioteka dla Studentów >>**" << std::endl << std::endl << std::endl;
-	std::cout << "Opcje :\n";
-	std::cout << "1 - Zaloguj siê		2 - Zarejestruj siê " << std::endl;
+	std::cout << "Opcje :" << std::endl;
+	std::cout << "[1] - Zaloguj siê\t\t[2] - Zarejestruj siê " << std::endl;
 	int decide;
 	std::cin >> decide;
 	switch (decide)
@@ -41,7 +42,7 @@ void userInterfaceUzytkownik()
 		userInterfaceZarejestrujSie();
 		break;
 	default:
-		std::cout << "Niepoprawne dane, spróbuj jeszcze raz" << std::endl;
+		std::cout << "Opcja o id: " << decide << " nie istnieje! Podaj w³aœciw¹ opcjê!" << std::endl;
 		system("pause");
 		userInterfaceUzytkownik();
 	}
@@ -65,7 +66,7 @@ void userInterfacePracownik()
 		
 		break;
 	default:
-		std::cout << "Niepoprawne dane, spróbuj jeszcze raz" << std::endl;
+		std::cout << "Opcja o id: " << decide << " nie istnieje! Podaj w³aœciw¹ opcjê!" << std::endl;
 		system("pause");
 		userInterfacePracownik();
 	}
@@ -74,6 +75,7 @@ void userInterfacePracownik()
 void userInterfaceZarejestrujSie()
 {
 	Czytelnik czytelnik;
+<<<<<<< HEAD
 	char input[256];
 	system("CLS");
 	std::cout << "**<< Biblioteka dla Studentów >>**" << std::endl << std::endl << std::endl;
@@ -85,22 +87,123 @@ void userInterfaceZarejestrujSie()
 	std::string nazwisko;
 	std::getline(std::cin, nazwisko);
 	czytelnik.setNazwisko(nazwisko);
+=======
+	bool zawieraSpacje;
+	bool wlasciweDane;
+	int decyzja;
+	std::string imie;
+	std::string nazwisko;
+BEGIN:
+	system("CLS");
+	std::cout << "**<< Biblioteka dla Studentów >>**" << std::endl << std::endl << std::endl;
+	if (imie.empty())
+	{
+		std::cout << "Podaj Imiê: ";
+		std::cin.ignore();
+		std::getline(std::cin, imie);
+		zawieraSpacje = false;
+		for (char c : imie) {
+			if (c == ' ') {
+				zawieraSpacje = true;
+				break;
+			}
+		}
+		if (zawieraSpacje)
+		{
+			std::cout << "Imiê które poda³eœ jest niepoprawne! Nie mo¿esz u¿ywaæ spacji!" << std::endl;
+			system("pause");
+			imie = "";
+		}
+		else
+		{
+			std::cout << "Czy " << imie << " to na pewno twoje imie?\n[1] Tak\t\t[2] Nie" << std::endl;
+			wlasciweDane = false;
+			while (!wlasciweDane)
+			{
+				std::cin >> decyzja;
+				switch (decyzja)
+				{
+				case 1: czytelnik.setImie(imie); wlasciweDane = true; break;
+				case 2: imie = "";  wlasciweDane = true; break;
+				default: std::cout << "Opcja o id: " << decyzja << " nie istnieje! Podaj w³aœciw¹ opcjê!" << std::endl;
+				}
+			}
+			
+			
+		}
+		goto BEGIN;
+	}
+	if (nazwisko.empty())
+	{
+		std::cout << "Podaj Nazwisko: ";
+		std::cin.ignore();
+		std::getline(std::cin, nazwisko);
+		zawieraSpacje = false;
+		for (char c : nazwisko) {
+			if (c == ' ') {
+				zawieraSpacje = true;
+				break;
+			}
+		}
+		if (zawieraSpacje)
+		{
+			std::cout << "Nazwisko które poda³eœ jest niepoprawne! Nie mo¿esz u¿ywaæ spacji!" << std::endl;
+			system("pause");
+			nazwisko = "";
+		}
+		else
+		{
+			std::cout << "Czy " << nazwisko << " to na pewno twoje nazwisko?\n[1] Tak\t\t[2] Nie" << std::endl;
+			wlasciweDane = false;
+			while (!wlasciweDane)
+			{
+				std::cin >> decyzja;
+				switch (decyzja)
+				{
+				case 1: czytelnik.setNazwisko(nazwisko); wlasciweDane = true; break;
+				case 2: nazwisko = "";  wlasciweDane = true; break;
+				default: std::cout << "Opcja o id: " << decyzja << " nie istnieje! Podaj w³aœciw¹ opcjê!" << std::endl;
+				}
+			}
+		}
+		goto BEGIN;
+	}
+>>>>>>> dc8a578c3a174e8aabda258310b8671fec0aab27
 	std::cout << "Podaj Adres:";
+	std::cin.ignore();
 	std::string adres;
 	std::getline(std::cin, adres);
 	czytelnik.setAdres(adres);
 	std::cout << "Podaj Datê Urodzenia:";
 	std::string dataUrodzenia;
-	std::cin >> dataUrodzenia;
-	//std::cout << dataUrodzenia;
+	std::getline(std::cin, dataUrodzenia);
 	czytelnik.setDataUrodzenia(dataUrodzenia); //jeœli wprowadzi siê adres z numerem to przeskakuje wprowadzanie daty urodzenia
 	std::cout << "Podaj Telefon:";
 	std::string telefon;
-	std::cin >> telefon;
+	std::getline(std::cin, telefon);
 	czytelnik.setTelefon(telefon);
 	std::cout << "Podaj PESEL:";
 	std::string pesel;
-	std::cin >> pesel;
+	while (true)
+	{
+		std::cin >> pesel;
+		if (pesel.length() != PESELLENGTH) {
+			std::cout << "Niepoprawna dlugosc numeru PESEL. Wprowadz 11 znakow." << std::endl;
+		}
+		else if (!std::all_of(pesel.begin(), pesel.end(), ::isdigit)) {
+			std::cout << "Numer PESEL powinien zawierac tylko cyfry." << std::endl;
+		}
+		else {
+			long long pesel_lenght = std::stoll(pesel);
+			if (pesel_lenght > MAXPESEL || pesel_lenght < MINPESEL) {
+				std::cout << "Niepoprawny nr PESEL. Numer PESEL powinien zawierac 11 cyfr." << std::endl;
+			}
+			else {
+				break;
+			}
+		}
+		std::cout << "Wprowadz nowy PESEL: ";
+	}
 	czytelnik.setPESEL(pesel);
 	std::cout << "Podaj Has³o:";
 	std::string haslo;
