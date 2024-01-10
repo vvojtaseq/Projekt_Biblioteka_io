30bcf19d36fc17be81bbde5159d9cfb73b511d64
WojZacharski <wojzacharski@student.agh.edu.pl>
2024-01-10 18:42:19 +0100

    dodanie getKartaBibliotecznaToFiles

--- Projekt_Biblioteka_io/Projekt_Biblioteka_io/KartaBiblioteczna.h
+++ Projekt_Biblioteka_io/Projekt_Biblioteka_io/KartaBiblioteczna.h
@@ -31,6 +31,36 @@ public:
 			plik.close();
 	}
 
+	//pobieranie kart bibliotecznych z pliku
+	KartaBiblioteczna getKartaBibliotecznaFromFiles(int id)
+	{
+		std::fstream plik;
+		plik.open("kartabiblioteczne.txt", std::ios::in);
+		std::string linia;
+		std::string idString;
+		std::string idKsiazkiString;
+		std::string dataWypozyczenia;
+		std::string dataZwrotu;
+		KartaBiblioteczna kartaBiblioteczna;
+		while (getline(plik, linia))
+		{
+			std::stringstream ss(linia);
+			getline(ss, idString, '|');
+			getline(ss, idKsiazkiString, '|');
+			getline(ss, dataWypozyczenia, '|');
+			getline(ss, dataZwrotu, '|');
+			if (id == std::stoi(idString))
+			{
+				kartaBiblioteczna.setIDuzytkownika(std::stoi(idString));
+				kartaBiblioteczna.setIDksiazki(std::stoi(idKsiazkiString));
+				kartaBiblioteczna.setDataWypozyczenia(dataWypozyczenia);
+				kartaBiblioteczna.setDataZwrotu(dataZwrotu);
+				return kartaBiblioteczna;
+			}
+		}
+		plik.close();
+		return kartaBiblioteczna;
+	}
 	
 };
 
